d466a708e046f4767c8da9b0264eae69c3dcb257
jpascuasd <jdavidpascuas@gmail.com>
2020-04-28 14:47:38 -0500

    primer codigo

---
+++ src/test/java/com/themakers/storeonline/web/rest/ClienteResourceIT.java
@@ -0,0 +1,231 @@
+package com.themakers.storeonline.web.rest;
+
+import com.themakers.storeonline.GatewaystoreApp;
+import com.themakers.storeonline.domain.Cliente;
+import com.themakers.storeonline.repository.ClienteRepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Integration tests for the {@link ClienteResource} REST controller.
+ */
+@SpringBootTest(classes = GatewaystoreApp.class)
+
+@AutoConfigureMockMvc
+@WithMockUser
+public class ClienteResourceIT {
+
+    private static final String DEFAULT_NOMBRE = "AAAAAAAAAA";
+    private static final String UPDATED_NOMBRE = "BBBBBBBBBB";
+
+    private static final String DEFAULT_APELLIDO = "AAAAAAAAAA";
+    private static final String UPDATED_APELLIDO = "BBBBBBBBBB";
+
+    private static final String DEFAULT_IDENTIFICACION = "AAAAAAAAAA";
+    private static final String UPDATED_IDENTIFICACION = "BBBBBBBBBB";
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private MockMvc restClienteMockMvc;
+
+    private Cliente cliente;
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Cliente createEntity(EntityManager em) {
+        Cliente cliente = new Cliente()
+            .nombre(DEFAULT_NOMBRE)
+            .apellido(DEFAULT_APELLIDO)
+            .identificacion(DEFAULT_IDENTIFICACION);
+        return cliente;
+    }
+    /**
+     * Create an updated entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Cliente createUpdatedEntity(EntityManager em) {
+        Cliente cliente = new Cliente()
+            .nombre(UPDATED_NOMBRE)
+            .apellido(UPDATED_APELLIDO)
+            .identificacion(UPDATED_IDENTIFICACION);
+        return cliente;
+    }
+
+    @BeforeEach
+    public void initTest() {
+        cliente = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createCliente() throws Exception {
+        int databaseSizeBeforeCreate = clienteRepository.findAll().size();
+
+        // Create the Cliente
+        restClienteMockMvc.perform(post("/api/clientes")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(cliente)))
+            .andExpect(status().isCreated());
+
+        // Validate the Cliente in the database
+        List<Cliente> clienteList = clienteRepository.findAll();
+        assertThat(clienteList).hasSize(databaseSizeBeforeCreate + 1);
+        Cliente testCliente = clienteList.get(clienteList.size() - 1);
+        assertThat(testCliente.getNombre()).isEqualTo(DEFAULT_NOMBRE);
+        assertThat(testCliente.getApellido()).isEqualTo(DEFAULT_APELLIDO);
+        assertThat(testCliente.getIdentificacion()).isEqualTo(DEFAULT_IDENTIFICACION);
+    }
+
+    @Test
+    @Transactional
+    public void createClienteWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = clienteRepository.findAll().size();
+
+        // Create the Cliente with an existing ID
+        cliente.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restClienteMockMvc.perform(post("/api/clientes")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(cliente)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Cliente in the database
+        List<Cliente> clienteList = clienteRepository.findAll();
+        assertThat(clienteList).hasSize(databaseSizeBeforeCreate);
+    }
+
+
+    @Test
+    @Transactional
+    public void getAllClientes() throws Exception {
+        // Initialize the database
+        clienteRepository.saveAndFlush(cliente);
+
+        // Get all the clienteList
+        restClienteMockMvc.perform(get("/api/clientes?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(cliente.getId().intValue())))
+            .andExpect(jsonPath("$.[*].nombre").value(hasItem(DEFAULT_NOMBRE)))
+            .andExpect(jsonPath("$.[*].apellido").value(hasItem(DEFAULT_APELLIDO)))
+            .andExpect(jsonPath("$.[*].identificacion").value(hasItem(DEFAULT_IDENTIFICACION)));
+    }
+    
+    @Test
+    @Transactional
+    public void getCliente() throws Exception {
+        // Initialize the database
+        clienteRepository.saveAndFlush(cliente);
+
+        // Get the cliente
+        restClienteMockMvc.perform(get("/api/clientes/{id}", cliente.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.id").value(cliente.getId().intValue()))
+            .andExpect(jsonPath("$.nombre").value(DEFAULT_NOMBRE))
+            .andExpect(jsonPath("$.apellido").value(DEFAULT_APELLIDO))
+            .andExpect(jsonPath("$.identificacion").value(DEFAULT_IDENTIFICACION));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingCliente() throws Exception {
+        // Get the cliente
+        restClienteMockMvc.perform(get("/api/clientes/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateCliente() throws Exception {
+        // Initialize the database
+        clienteRepository.saveAndFlush(cliente);
+
+        int databaseSizeBeforeUpdate = clienteRepository.findAll().size();
+
+        // Update the cliente
+        Cliente updatedCliente = clienteRepository.findById(cliente.getId()).get();
+        // Disconnect from session so that the updates on updatedCliente are not directly saved in db
+        em.detach(updatedCliente);
+        updatedCliente
+            .nombre(UPDATED_NOMBRE)
+            .apellido(UPDATED_APELLIDO)
+            .identificacion(UPDATED_IDENTIFICACION);
+
+        restClienteMockMvc.perform(put("/api/clientes")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(updatedCliente)))
+            .andExpect(status().isOk());
+
+        // Validate the Cliente in the database
+        List<Cliente> clienteList = clienteRepository.findAll();
+        assertThat(clienteList).hasSize(databaseSizeBeforeUpdate);
+        Cliente testCliente = clienteList.get(clienteList.size() - 1);
+        assertThat(testCliente.getNombre()).isEqualTo(UPDATED_NOMBRE);
+        assertThat(testCliente.getApellido()).isEqualTo(UPDATED_APELLIDO);
+        assertThat(testCliente.getIdentificacion()).isEqualTo(UPDATED_IDENTIFICACION);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingCliente() throws Exception {
+        int databaseSizeBeforeUpdate = clienteRepository.findAll().size();
+
+        // Create the Cliente
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restClienteMockMvc.perform(put("/api/clientes")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(cliente)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Cliente in the database
+        List<Cliente> clienteList = clienteRepository.findAll();
+        assertThat(clienteList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    public void deleteCliente() throws Exception {
+        // Initialize the database
+        clienteRepository.saveAndFlush(cliente);
+
+        int databaseSizeBeforeDelete = clienteRepository.findAll().size();
+
+        // Delete the cliente
+        restClienteMockMvc.perform(delete("/api/clientes/{id}", cliente.getId())
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+
+        // Validate the database contains one less item
+        List<Cliente> clienteList = clienteRepository.findAll();
+        assertThat(clienteList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+}
---
+++ src/test/java/com/themakers/storeonline/web/rest/UsuarioResourceIT.java
@@ -0,0 +1,242 @@
+package com.themakers.storeonline.web.rest;
+
+import com.themakers.storeonline.GatewaystoreApp;
+import com.themakers.storeonline.domain.Usuario;
+import com.themakers.storeonline.domain.User;
+import com.themakers.storeonline.repository.UsuarioRepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Integration tests for the {@link UsuarioResource} REST controller.
+ */
+@SpringBootTest(classes = GatewaystoreApp.class)
+
+@AutoConfigureMockMvc
+@WithMockUser
+public class UsuarioResourceIT {
+
+    private static final String DEFAULT_NOMBRE = "AAAAAAAAAA";
+    private static final String UPDATED_NOMBRE = "BBBBBBBBBB";
+
+    private static final String DEFAULT_APELLIDO = "AAAAAAAAAA";
+    private static final String UPDATED_APELLIDO = "BBBBBBBBBB";
+
+    private static final String DEFAULT_DIRECCION = "AAAAAAAAAA";
+    private static final String UPDATED_DIRECCION = "BBBBBBBBBB";
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private MockMvc restUsuarioMockMvc;
+
+    private Usuario usuario;
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Usuario createEntity(EntityManager em) {
+        Usuario usuario = new Usuario()
+            .nombre(DEFAULT_NOMBRE)
+            .apellido(DEFAULT_APELLIDO)
+            .direccion(DEFAULT_DIRECCION);
+        // Add required entity
+        User user = UserResourceIT.createEntity(em);
+        em.persist(user);
+        em.flush();
+        usuario.setUser(user);
+        return usuario;
+    }
+    /**
+     * Create an updated entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Usuario createUpdatedEntity(EntityManager em) {
+        Usuario usuario = new Usuario()
+            .nombre(UPDATED_NOMBRE)
+            .apellido(UPDATED_APELLIDO)
+            .direccion(UPDATED_DIRECCION);
+        // Add required entity
+        User user = UserResourceIT.createEntity(em);
+        em.persist(user);
+        em.flush();
+        usuario.setUser(user);
+        return usuario;
+    }
+
+    @BeforeEach
+    public void initTest() {
+        usuario = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createUsuario() throws Exception {
+        int databaseSizeBeforeCreate = usuarioRepository.findAll().size();
+
+        // Create the Usuario
+        restUsuarioMockMvc.perform(post("/api/usuarios")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(usuario)))
+            .andExpect(status().isCreated());
+
+        // Validate the Usuario in the database
+        List<Usuario> usuarioList = usuarioRepository.findAll();
+        assertThat(usuarioList).hasSize(databaseSizeBeforeCreate + 1);
+        Usuario testUsuario = usuarioList.get(usuarioList.size() - 1);
+        assertThat(testUsuario.getNombre()).isEqualTo(DEFAULT_NOMBRE);
+        assertThat(testUsuario.getApellido()).isEqualTo(DEFAULT_APELLIDO);
+        assertThat(testUsuario.getDireccion()).isEqualTo(DEFAULT_DIRECCION);
+    }
+
+    @Test
+    @Transactional
+    public void createUsuarioWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = usuarioRepository.findAll().size();
+
+        // Create the Usuario with an existing ID
+        usuario.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restUsuarioMockMvc.perform(post("/api/usuarios")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(usuario)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Usuario in the database
+        List<Usuario> usuarioList = usuarioRepository.findAll();
+        assertThat(usuarioList).hasSize(databaseSizeBeforeCreate);
+    }
+
+
+    @Test
+    @Transactional
+    public void getAllUsuarios() throws Exception {
+        // Initialize the database
+        usuarioRepository.saveAndFlush(usuario);
+
+        // Get all the usuarioList
+        restUsuarioMockMvc.perform(get("/api/usuarios?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(usuario.getId().intValue())))
+            .andExpect(jsonPath("$.[*].nombre").value(hasItem(DEFAULT_NOMBRE)))
+            .andExpect(jsonPath("$.[*].apellido").value(hasItem(DEFAULT_APELLIDO)))
+            .andExpect(jsonPath("$.[*].direccion").value(hasItem(DEFAULT_DIRECCION)));
+    }
+    
+    @Test
+    @Transactional
+    public void getUsuario() throws Exception {
+        // Initialize the database
+        usuarioRepository.saveAndFlush(usuario);
+
+        // Get the usuario
+        restUsuarioMockMvc.perform(get("/api/usuarios/{id}", usuario.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.id").value(usuario.getId().intValue()))
+            .andExpect(jsonPath("$.nombre").value(DEFAULT_NOMBRE))
+            .andExpect(jsonPath("$.apellido").value(DEFAULT_APELLIDO))
+            .andExpect(jsonPath("$.direccion").value(DEFAULT_DIRECCION));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingUsuario() throws Exception {
+        // Get the usuario
+        restUsuarioMockMvc.perform(get("/api/usuarios/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateUsuario() throws Exception {
+        // Initialize the database
+        usuarioRepository.saveAndFlush(usuario);
+
+        int databaseSizeBeforeUpdate = usuarioRepository.findAll().size();
+
+        // Update the usuario
+        Usuario updatedUsuario = usuarioRepository.findById(usuario.getId()).get();
+        // Disconnect from session so that the updates on updatedUsuario are not directly saved in db
+        em.detach(updatedUsuario);
+        updatedUsuario
+            .nombre(UPDATED_NOMBRE)
+            .apellido(UPDATED_APELLIDO)
+            .direccion(UPDATED_DIRECCION);
+
+        restUsuarioMockMvc.perform(put("/api/usuarios")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(updatedUsuario)))
+            .andExpect(status().isOk());
+
+        // Validate the Usuario in the database
+        List<Usuario> usuarioList = usuarioRepository.findAll();
+        assertThat(usuarioList).hasSize(databaseSizeBeforeUpdate);
+        Usuario testUsuario = usuarioList.get(usuarioList.size() - 1);
+        assertThat(testUsuario.getNombre()).isEqualTo(UPDATED_NOMBRE);
+        assertThat(testUsuario.getApellido()).isEqualTo(UPDATED_APELLIDO);
+        assertThat(testUsuario.getDireccion()).isEqualTo(UPDATED_DIRECCION);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingUsuario() throws Exception {
+        int databaseSizeBeforeUpdate = usuarioRepository.findAll().size();
+
+        // Create the Usuario
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restUsuarioMockMvc.perform(put("/api/usuarios")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(usuario)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Usuario in the database
+        List<Usuario> usuarioList = usuarioRepository.findAll();
+        assertThat(usuarioList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    public void deleteUsuario() throws Exception {
+        // Initialize the database
+        usuarioRepository.saveAndFlush(usuario);
+
+        int databaseSizeBeforeDelete = usuarioRepository.findAll().size();
+
+        // Delete the usuario
+        restUsuarioMockMvc.perform(delete("/api/usuarios/{id}", usuario.getId())
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+
+        // Validate the database contains one less item
+        List<Usuario> usuarioList = usuarioRepository.findAll();
+        assertThat(usuarioList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+}
